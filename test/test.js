7fbc5b26c081be6c0636d85693349c26739200e1
MathieuAuclair <mathiass12@hotmail.com>
2017-09-07 01:17:01 -0400

    prepare testing + move lost module

--- test/test.js
+++ test/test.js
@@ -1,2 +1,20 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const token = process.env.MOISTY;
+const moduleManager = require("../modules/manager.js");
+
+console.log("MAKE SURE MOISTY IS OFFLINE!");
+
+client.login(token);
+
+client.on('ready', () => {
+	//confirm login
+	console.log("MOISTY:200");
+	//terminate the session
+	process.exit();
+});
+
+
 
 
