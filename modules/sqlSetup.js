f4f6abca1fbeb5e8435719a4b8fd2cb4700bf78a
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2017-09-09 22:03:27 -0400

    Adds docker-compose and improve readme :v:

--- modules/sqlSetup.js
+++ modules/sqlSetup.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 
 function setup(){
 	this.connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password : process.env.PASSWORD,
-		database : 'DISCORD'
+		host: process.env.MYSQL_HOST,
+		user: process.env.MYSQL_USER,
+		password : process.env.MYSQL_PASSWORD,
+		database : process.env.MYSQL_DATABASE
 	}),
 	this.connect = function(){
 		this.connection.connect(function(err){
