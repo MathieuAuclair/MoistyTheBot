6322a597c9394da0be41993be40b0b0e07494fe3
MathieuAuclair <mathiass12@hotmail.com>
2017-09-10 23:07:57 -0400

    match env var to pass test

--- modules/sqlPool.js
+++ modules/sqlPool.js
@@ -6,10 +6,10 @@ module.exports = {
     getPool: function () {
       if (pool) return pool;
       pool = mysql.createPool({
-        host     : 'localhost',
-        user     : 'root',
-        password : process.env.PASSWORD,
-        database : 'DISCORD'
+        host     : process.env.MYSQL_HOST,
+        user     : process.env.MYSQL_USER,
+        password : process.env.MYSQL_PASSWORD,
+        database : process.env.MYSQL_DATABASE
       });
       return pool;
     }
